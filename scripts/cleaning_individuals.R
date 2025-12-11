91a41716e3cc723b6e005fa7135f17a7b104cf8f
REBOURS Anthony <reboursanthony@hotmail.fr>
2025-12-11 15:38:17 +0100

    codes traitement noms des colonnes table individus

--- scripts/cleaning_individuals.R
+++ scripts/cleaning_individuals.R
@@ -1,8 +1,19 @@
+##%######################################################%##
+#                                                          #
+####       Traitement données individus datagouv        ####
+#                                                          #
+##%######################################################%##
 
+
+library(tidyverse)
+library(janitor)
+
+individuals <- arrow::read_parquet(here::here("data", "datagouv_individuals.parquet"))
 
 
 # Fusion nom et prenom --------------------------------------------------------
 # Extraction des préfixes complets
+cols <- names(individuals)
 prefixes <- unique(sub("\\.(nom|prenom)$", "", cols))
 
 # Pour chaque préfixe, fusionner les colonnes ayant ".nom" et ".prenom" en suffixe
@@ -11,8 +22,8 @@ for (prefix in prefixes) {
   prenom_col <- paste0(prefix, ".prenom")
   
   if (nom_col %in% cols && prenom_col %in% cols) {
-    theses <- 
-      theses %>% 
+    individuals <- 
+      individuals %>% 
       unite(
         !!sym(prefix),
         c(!!sym(nom_col), !!sym(prenom_col)),
@@ -22,8 +33,26 @@ for (prefix in prefixes) {
   }
 }
 
-theses <- 
-  theses %>% 
+cols <- names(individuals)
+for (prefix in prefixes) {
+  idref_col <- paste0(prefix, ".idref")
+  individu_col <- prefix
+  
+  if (idref_col %in% cols && individu_col %in% cols) {
+    individuals <- 
+      individuals %>% 
+      unite(
+        !!sym(prefix),
+        c(!!sym(idref_col), !!sym(individu_col)),
+        sep = "|",
+        na.rm = TRUE
+      )
+  }
+}
+
+
+individuals <- 
+  individuals %>% 
   mutate(
     across(
       contains("idref"),
@@ -35,4 +64,5 @@ theses <-
     )
   )
 
-# Etablissements de soutenance
+
+
