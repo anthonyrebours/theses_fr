35329129e83c78f9ad909f72aadd4c211b8d086f
REBOURS Anthony <reboursanthony@hotmail.fr>
2025-12-12 12:04:57 +0100

    ajout codes pour restructurer table individuals

--- scripts/cleaning_individuals.R
+++ scripts/cleaning_individuals.R
@@ -5,14 +5,15 @@
 ##%######################################################%##
 
 
+# Packages et données -----------------------------------------------------
 library(tidyverse)
 library(janitor)
 
 individuals <- arrow::read_parquet(here::here("data", "datagouv_individuals.parquet"))
 
 
-# Fusion nom et prenom --------------------------------------------------------
-# Extraction des préfixes complets
+# Fusion des variables individuelles --------------------------------------
+## Extraction des préfixes complets
 cols <- names(individuals)
 prefixes <- unique(sub("\\.(nom|prenom)$", "", cols))
 
@@ -33,36 +34,68 @@ for (prefix in prefixes) {
   }
 }
 
-cols <- names(individuals)
+## Nouvelles variables
+cols_2 <- names(individuals)
+
+## Fusion nom et .idref
 for (prefix in prefixes) {
   idref_col <- paste0(prefix, ".idref")
   individu_col <- prefix
   
-  if (idref_col %in% cols && individu_col %in% cols) {
+  if (idref_col %in% cols_2 && individu_col %in% cols_2) {
     individuals <- 
       individuals %>% 
       unite(
         !!sym(prefix),
-        c(!!sym(idref_col), !!sym(individu_col)),
+        c(!!sym(individu_col), !!sym(idref_col)),
         sep = "|",
         na.rm = TRUE
       )
   }
 }
 
-
+## 
 individuals <- 
   individuals %>% 
   mutate(
     across(
-      contains("idref"),
-      ~ ifelse(
-        !is.na(.), 
-        paste0("(",.,")"), 
-        NA_real_
-      )
+      matches("directeurs|membres|president|rapporteurs"),
+      ~ na_if(.,"")
     )
   )
+  
+##  Format long 
+individuals <- 
+  individuals %>% 
+  pivot_longer(
+    cols = auteur:rapporteurs.5,
+    names_to = "status",
+    values_to = "noms",
+    values_drop_na = TRUE
+  ) 
 
+## Correction nom de variables en tant qu'observations
+individuals <- 
+  individuals %>%
+  mutate(status = str_remove(status, "\\..*")) 
+
+## Separation noms et idref
+individuals <-
+  individuals %>%
+  separate(noms, c("noms", "idref"), sep = "\\|")
+
+# individuals <- 
+#   individuals %>% 
+#   mutate(
+#     across(
+#       contains("idref"),
+#       ~ ifelse(
+#         !is.na(.), 
+#         paste0("(",.,")"), 
+#         NA_real_
+#       )
+#     )
+#   )
+# 
 
 
